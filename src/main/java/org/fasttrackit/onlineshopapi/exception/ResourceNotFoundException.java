ae2cc3fe9ff1793732961535286f84249985e3cb
REZA <alirezaseidi@hotmail.com>
2019-04-04 21:36:37 +0300

    studied custom exceptions; continued CRUD service; updated configs

---
+++ src/main/java/org/fasttrackit/onlineshopapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,4 @@
+package org.fasttrackit.onlineshopapi.exception;
+
+public class ResourceNotFoundEXception {
+}
--- src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
@@ -3,13 +3,20 @@ package org.fasttrackit.onlineshopapi.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.fasttrackit.onlineshopapi.domain.Product;
+import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshopapi.persistence.ProductRepository;
 import org.fasttrackit.onlineshopapi.transfer.CreateProductRequest;
+import org.fasttrackit.onlineshopapi.transfer.UpdateProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ProductService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 
     private final ProductRepository productRepository;
     private final ObjectMapper objectMapper;
@@ -21,8 +28,31 @@ public class ProductService {
     }
 
     public Product createProduct(CreateProductRequest request) {
+        LOGGER.info("Creating product {}", request);
         Product product = objectMapper.convertValue(request, Product.class);
         return productRepository.save(product);
     }
 
+    public Product getProduct(long id) throws ResourceNotFoundException {
+        LOGGER.info("Retrieving product {}", id);
+
+        return productRepository.findById(id)
+                // Optional and lambda expression
+                .orElseThrow(() -> new ResourceNotFoundException("Product " + id + " not found"));
+    }
+
+    public Product updateProduct(Long id, UpdateProductRequest request) throws ResourceNotFoundException {
+        LOGGER.info("Updateing product {}, {}", id, request);
+        Product product = getProduct(id);
+
+        BeanUtils.copyProperties(request, product);
+
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        LOGGER.info("Deleted product {}", id);
+        productRepository.deleteById(id);
+        LOGGER.info("Deleted product {}", id);
+    }
 }
